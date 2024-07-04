a9f7ddd7c6e145ac4fcb069ae1610e1296268e9f
ma5d <2689169757@qq.com>
2024-07-04 23:20:01 +0800

    first commit

---
+++ src/main/java/org/ma5d/onlymyfiles/ShowProjectNameAction.java
@@ -0,0 +1,12 @@
+package org.ma5d.onlymyfiles;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class ShowProjectNameAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
