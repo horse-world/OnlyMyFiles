83dea531b5e979d7427724408f7c2d4d0c260e36
ma5d <2689169757@qq.com>
2024-07-04 23:20:15 +0800

    first commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+# 用于在git中只展示本人参与的commit记录的脚本
+## 1. 项目初始化
+https://www.cnblogs.com/moutory/p/17752679.html
--- src/main/java/org/ma5d/onlymyfiles/ShowProjectNameAction.java
+++ src/main/java/org/ma5d/onlymyfiles/ShowProjectNameAction.java
@@ -2,11 +2,15 @@ package org.ma5d.onlymyfiles;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
 
 public class ShowProjectNameAction extends AnAction {
 
     @Override
     public void actionPerformed(AnActionEvent e) {
         // TODO: insert action logic here
+        Project project = e.getProject();
+        Messages.showMessageDialog("项目名称为" + project.getName(), "这是标题", Messages.getInformationIcon());
     }
 }
