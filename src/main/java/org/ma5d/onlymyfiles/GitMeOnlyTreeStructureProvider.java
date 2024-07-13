dc4ad2296b001985ad1349335fb3c37fd9fc7f5c
ma5d <2689169757@qq.com>
2024-07-14 01:05:22 +0800

    文件过滤逻辑

--- src/main/java/org/ma5d/onlymyfiles/GitMeOnlyTreeStructureProvider.java
+++ src/main/java/org/ma5d/onlymyfiles/GitMeOnlyTreeStructureProvider.java
@@ -1,49 +1,73 @@
 package org.ma5d.onlymyfiles;
 
-import com.intellij.icons.ExpUiIcons;
 import com.intellij.ide.projectView.TreeStructureProvider;
 import com.intellij.ide.projectView.ViewSettings;
 import com.intellij.ide.projectView.impl.nodes.BasePsiNode;
 import com.intellij.ide.projectView.impl.nodes.PsiDirectoryNode;
 import com.intellij.ide.projectView.impl.nodes.PsiFileNode;
 import com.intellij.ide.util.treeView.AbstractTreeNode;
-import com.intellij.openapi.fileTypes.PlainTextFileType;
-import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.stream.Collectors;
-// import org.jetbrains.plugins.gradle.projectView.GradleTreeStructureProvider;
+import java.util.*;
 
 public class GitMeOnlyTreeStructureProvider implements TreeStructureProvider {
 
+    ArrayList<AbstractTreeNode<?>> nodes = new ArrayList<>();
+    String userName = "ma";
+
     @Override
     public @NotNull Collection<AbstractTreeNode<?>> modify(@NotNull AbstractTreeNode<?> parent, @NotNull Collection<AbstractTreeNode<?>> children, ViewSettings settings) {
-        ArrayList<AbstractTreeNode<?>> nodes = new ArrayList<>();
+
+        try {
+            BufferedReader reader = Runtime.getRuntime().exec("git config --get user.name").inputReader();
+            userName = reader.readLine();
+            reader.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
         for (AbstractTreeNode<?> child : children) {
-            if(!(child instanceof BasePsiNode)){
+            if (!(child instanceof BasePsiNode)) {
                 continue;
             }
-            if(child instanceof PsiDirectoryNode){
-                dfs(nodes, (PsiDirectoryNode) child);
+            if (child instanceof PsiDirectoryNode) {
+                try {
+                    String path = Objects.requireNonNull(((PsiDirectoryNode) child).getVirtualFile()).getPath();
+                    String name = child.getProject().getName();
+                    if (!path.contains(name)) continue;
+                    if (path.contains("build")) continue;
+                    dfs(nodes, (PsiDirectoryNode) child);
+                } catch (NullPointerException | IOException e) {
+                    throw new RuntimeException(e);
+                }
             }
             nodes.add(child);
         }
         return nodes;
     }
 
+    @SuppressWarnings("resource")
     private void dfs(ArrayList<AbstractTreeNode<?>> nodes, PsiDirectoryNode children) throws IOException {
         for (AbstractTreeNode<?> child : children.getChildren()) {
-            BufferedReader reader = Runtime.getRuntime().exec("git config --get user.name").inputReader();
-            String userName = reader.readLine();
-            reader.close();
+            String path = Objects.requireNonNull(children.getVirtualFile()).getPath();
+            if (child instanceof PsiFileNode) {
+                List<String> list = new ArrayList<>(Arrays.stream(path.split("/")).toList());
+                list.remove(list.size() - 1);
+                String directory = String.join("/", list);
+                List<String> res = Runtime.getRuntime().exec(String.format("cd %s && git log --author=%s %s", directory, userName, path)).inputReader().lines().toList();
+                if (res.isEmpty()) return;
+                nodes.add(child);
+            }
 
-            Runtime.getRuntime().exec("git config --get user.email").inputReader();
+            if (child instanceof PsiDirectoryNode) {
+                List<String> res = Runtime.getRuntime().exec(String.format("cd %s && git log --author=%s %s", path, userName, path)).inputReader().lines().toList();
+                if (res.isEmpty()) return;
+                nodes.add(child);
+                dfs(nodes, (PsiDirectoryNode) child);
+            }
+
         }
     }
 
